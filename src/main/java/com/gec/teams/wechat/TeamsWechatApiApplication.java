0a2ae0a8f1810d4a5a12b8d7d918e389b5c28bd2
xin-77 <1693991091@qq.com>
2023-07-03 15:10:56 +0800

    v0.1

---
+++ src/main/java/com/gec/teams/wechat/TeamsWechatApiApplication.java
@@ -0,0 +1,13 @@
+package com.gec.teams.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsWechatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamsWechatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gec/teams/wechat/TeamsWechatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gec.teams.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsWechatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
